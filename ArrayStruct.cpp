dd998413d6f881d7526ae2356347b887410a81e8
pasharyan <pasharyan50@gmail.com>
2026-04-06 11:42:37 +0700

    menampilkan data dengan perintah satu persatu

--- ArrayStruct.cpp
+++ ArrayStruct.cpp
@@ -27,3 +27,10 @@ int main()
     cin >> datamu[3];
     cout << "Isikan data kelima : ";
     cin >> datamu[4];
+
+    // menampilkan data dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
